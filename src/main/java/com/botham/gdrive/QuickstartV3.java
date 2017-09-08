cef663bcc018c2230dc9f9e445bf42de6be310c9
steve <steve@fncserver3>
2017-09-07 21:15:09 -0400

    1

--- src/main/java/com/botham/gdrive/QuickstartV3.java
+++ src/main/java/com/botham/gdrive/QuickstartV3.java
@@ -23,7 +23,6 @@ import java.util.Arrays;
 import java.util.List;
 import org.apache.log4j.Logger;
 
-
 public class QuickstartV3 {
 	
 	static Logger log = Logger.getLogger(QuickstartV3.class.getName());
