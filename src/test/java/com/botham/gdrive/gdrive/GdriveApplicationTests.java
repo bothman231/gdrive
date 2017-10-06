ac09de80d015095249d2a1f233feef1ce867ae65
steve <steve@fncserver3>
2017-10-05 21:26:05 -0400

    1

--- src/test/java/com/botham/gdrive/gdrive/GdriveApplicationTests.java
+++ src/test/java/com/botham/gdrive/gdrive/GdriveApplicationTests.java
@@ -1,10 +1,10 @@
 package com.botham.gdrive.gdrive;
 
+/*
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GdriveApplicationTests {
@@ -14,3 +14,4 @@ public class GdriveApplicationTests {
 	}
 
 }
+*/
